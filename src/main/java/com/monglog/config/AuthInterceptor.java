59f8f4d97829a37b45ee68f13ba2c0dc82a48e6f
cmong0516 <cmong0516@gmail.com>
2023-03-23 14:37:16 +0900

    2023.03.23

--- src/main/java/com/monglog/config/AuthInterceptor.java
+++ src/main/java/com/monglog/config/AuthInterceptor.java
@@ -1,4 +1,38 @@
 package com.monglog.config;
 
-public class AuthInterceptor {
+import com.monglog.exception.Unauthorized;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        log.info(">> preHandle");
+
+        String accessToken = request.getParameter("accessToken");
+
+        if (accessToken != null && accessToken.equals("hodolman")) {
+            return true;
+        }
+
+        throw new Unauthorized();
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        log.info(">> postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        log.info(">> afterCompletion");
+    }
 }
---
+++ src/main/java/com/monglog/exception/Unauthorized.java
@@ -0,0 +1,15 @@
+package com.monglog.exception;
+
+public class Unauthorized extends MongLogException{
+
+    private static final String MESSAGE = "인증이 필요합니다.";
+
+    public Unauthorized() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 401;
+    }
+}
