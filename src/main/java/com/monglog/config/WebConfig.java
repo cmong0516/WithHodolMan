37853ee84a3f0fd6af62d645a42d961d9664f14c
cmong0516 <cmong0516@gmail.com>
2023-03-23 14:39:13 +0900

    2023.03.23

--- src/main/java/com/monglog/config/WebConfig.java
+++ src/main/java/com/monglog/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.monglog.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .excludePathPatterns("/foo");
     }
 }
